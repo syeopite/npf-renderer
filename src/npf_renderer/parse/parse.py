9cf9546dfad42dd94b9890a1ffbf0f51e5fc0f30
syeopite <syeopite@syeopite.dev>
2022-10-06 02:55:47 -0700

    Parser: Rename content to content_array

--- src/npf_renderer/parse/parse.py
+++ src/npf_renderer/parse/parse.py
@@ -4,11 +4,11 @@ class Parser:
     TODO Make base class with all of these properties for the formatter and parser
     """
     def __init__(self, content):
-        self.content = content
+        self.content_array = content
         self.parsed_result = []
 
         self.cursor = 0
-        self.current = self.content[self.cursor]
+        self.current = self.content_array[self.cursor]
         self.content_length = len(content)
 
     @property
@@ -18,20 +18,23 @@ class Parser:
     def __next(self):
         """Moves cursor forward by one and returns the (now) selected element"""
         self.cursor += 1
-        self.current = self.content[self.cursor]
+        self.current = self.content_array[self.cursor]
         return self.current
 
     def _peek(self):
         """Takes a peek at the next element"""
         if self._at_end:
             return False
-        return self.content[self.cursor + 1]
+        return self.content_array[self.cursor + 1]
 
     def __prev(self):
         """Moves cursor back by one and returns the (now) selected element"""
         self.cursor -= 1
-        self.current = self.content[self.cursor]
+        self.current = self.content_array[self.cursor]
         return self.current
+
+    def _parse_text(self):
+        pass
 
     def parse(self):
         while not self._at_end:
