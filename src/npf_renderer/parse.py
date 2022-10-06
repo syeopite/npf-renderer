bde1a5d96832c9dc2608e824d0314b50ac625fa5
syeopite <syeopite@syeopite.dev>
2022-10-06 02:32:10 -0700

    Add skele. for an all-in-one parser for NPF blocks
    The current parser is far too basic and pushes the complexity to the
    renderer(s) instead. This aims to fix that by starting the work on a
    more complex parser that can handle nesting and other relationships
    within NPF blocks (although primary referring to text blocks in this
    case)

---
+++ src/npf_renderer/parse.py
@@ -0,0 +1,38 @@
+class Parser:
+    """All-in-one parser to process NPF content types
+
+    TODO Make base class with all of these properties for the formatter and parser
+    """
+    def __init__(self, content):
+        self.content = content
+        self.parsed_result = []
+
+        self.cursor = 0
+        self.current = self.content[self.cursor]
+        self.content_length = len(content)
+
+    @property
+    def _at_end(self):
+        return self.content_length - 1 == self.cursor
+
+    def __next(self):
+        """Moves cursor forward by one and returns the (now) selected element"""
+        self.cursor += 1
+        self.current = self.content[self.cursor]
+        return self.current
+
+    def _peek(self):
+        """Takes a peek at the next element"""
+        if self._at_end:
+            return False
+        return self.content[self.cursor + 1]
+
+    def __prev(self):
+        """Moves cursor back by one and returns the (now) selected element"""
+        self.cursor -= 1
+        self.current = self.content[self.cursor]
+        return self.current
+
+    def parse(self):
+        while not self._at_end:
+            pass
