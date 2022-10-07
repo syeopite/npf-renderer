9fa0c4cb2edef2c598ff96b1ba7f14b71261faa5
syeopite <syeopite@syeopite.dev>
2022-10-06 17:55:22 -0700

    Explicitly get values from example_data.py

--- tests/text/test_parse.py
+++ tests/text/test_parse.py
@@ -1,5 +1,4 @@
 from example_data import *
-import hashlib
 from npf_renderer.parse.parse import Parser
 
 import logging
@@ -17,39 +16,39 @@ def helper_function(raw, answer):
 
 
 def test_simple_text_parse():
-    helper_function(*simple_test)
+    helper_function(simple_test[0], simple_test[1])
 
 
 def test_empty_string_parse():
-    helper_function(*longer_and_with_empty_string_test)
+    helper_function(longer_and_with_empty_string_test[0], longer_and_with_empty_string_test[1])
 
 def test_subtype_string_parse():
-    helper_function(*subtype_string_test)
+    helper_function(subtype_string_test[0], subtype_string_test[1])
 
 
 def test_subtype_string_parse_2():
-    helper_function(*subtype_string_test_2)
+    helper_function(subtype_string_test_2[0], subtype_string_test_2[1])
 
 
 def test_subtype_and_indent_level_parse():
-    helper_function(*subtype_and_indent_level_test)
+    helper_function(subtype_and_indent_level_test[0], subtype_and_indent_level_test[1])
 
 
 def test_inline_formatting_parse():
-    helper_function(*inline_formatting_test)
+    helper_function(inline_formatting_test[0], inline_formatting_test[1])
 
 
 def test_inline_link_formatting_parse():
-    helper_function(*inline_formatting_link_test)
+    helper_function(inline_formatting_link_test[0], inline_formatting_link_test[1])
 
 
 def test_inline_mention_formatting_parse():
-    helper_function(*inline_formatting_mention_test)
+    helper_function(inline_formatting_mention_test[0], inline_formatting_mention_test[1])
 
 
 def test_inline_color_formatting_parse():
-    helper_function(*inline_formatting_color_test)
+    helper_function(inline_formatting_color_test[0], inline_formatting_color_test[1])
 
 
 def test_overlapping_inline_parse():
-    helper_function(*test_inline_overlapping)
+    helper_function(test_inline_overlapping[0], test_inline_overlapping[1])
