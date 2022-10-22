63e071e3edcf2eaf017ebb25e5734b41a84736c7
syeopite <syeopite@syeopite.dev>
2022-10-21 18:04:14 -0700

    Remove main.py and make __init__.py functional

--- src/main.py
+++
@@ -1,6 +0,0 @@
-from dominate import tags
-
-
-def render_content(content: list[dict], layout=None, trails=None):
-    post = tags.div(cls="post")
-    return post
---
+++ src/npf_renderer/__init__.py
@@ -0,0 +1,9 @@
+from . format import format_content
+from .parse import Parser
+
+
+def format_npf(contents, layouts=None, trails=None, url_handler=None):
+    parsed_contents = Parser(contents).parse()
+
+    return format_content(parsed_contents, layouts, trails, url_handler)
+
