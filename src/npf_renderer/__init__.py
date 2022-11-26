a80eec389f617acf269f79f830ef0fb58dbea69d
syeopite <syeopite@syeopite.dev>
2022-11-25 22:31:56 -0800

    Integrate layout parser with format_npf()

--- src/npf_renderer/__init__.py
+++ src/npf_renderer/__init__.py
@@ -1,9 +1,10 @@
 from .format import format_content
-from .parse import Parser
+from .parse import Parser, LayoutParser
 
 
 def format_npf(contents, layouts=None, trails=None, url_handler=None):
     parsed_contents = Parser(contents).parse()
+    parsed_layouts = LayoutParser(layouts).parse()
 
-    return format_content(parsed_contents, layouts, trails, url_handler)
+    return format_content(parsed_contents, parsed_layouts, trails, url_handler)
 
