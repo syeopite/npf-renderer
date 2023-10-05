e32d1adde9bf2a4412fec077b31dd2e78106e2f7
syeopite <syeopite@syeopite.dev>
2023-10-04 21:23:43 -0400

    Fallback to empty post-body div on error

--- src/npf_renderer/format_npf.py
+++ src/npf_renderer/format_npf.py
@@ -17,5 +17,10 @@ def format_npf(contents, layouts=None, *_, url_handler=None,
         contains_render_errors = True
         formatted = e.rendered_result
         assert formatted is not None
+    except Exception as e:
+        # TODO
+        # Something had gone wrong block
+        formatted = dominate.tags.div(cls="post-body")
+        contains_render_errors = True
 
     return contains_render_errors, formatted.render(pretty=pretty_html)
