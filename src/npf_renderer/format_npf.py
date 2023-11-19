97947001d93b51d4bdc3475a1f62a0063aac0900
syeopite <syeopite@syeopite.dev>
2023-11-18 16:35:40 -0800

    Add docstring to format_npf

--- src/npf_renderer/format_npf.py
+++ src/npf_renderer/format_npf.py
@@ -6,6 +6,18 @@ from . import exceptions
 
 
 def format_npf(contents, layouts=None, *_, url_handler=None, pretty_html=False):
+    """Formats the given NPF blocks into HTML
+    
+    Parameters:
+        contents: Array of npf blocks to be parsed.
+        layouts:  Layout information to arrange the NPF blocks into.
+
+        url_handler:
+            A function in which all URLs are passed into. Expects a string in return. 
+            By default the internal logic will default to lambda url : url
+        
+        pretty_html: Whether or not to render human readable html 
+    """
     contents = Parser(contents).parse()
     if layouts:
         layouts = LayoutParser(layouts).parse()
