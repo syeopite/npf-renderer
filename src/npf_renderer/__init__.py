09e40dbbfa75a92d4bd2efb399ed566af6d9f04d
syeopite <syeopite@syeopite.dev>
2024-12-28 17:36:28 -0800

    Add param to modify locale strings in format_npf

--- src/npf_renderer/__init__.py
+++ src/npf_renderer/__init__.py
@@ -1,6 +1,9 @@
 from npf_renderer import utils
+from npf_renderer.format.i18n import DEFAULT_LOCALIZATION
 from .format_npf import format_npf
 
 from . import exceptions, parse, objects, format
+
+DEFAULT_LOCALIZATION = format.i18n.DEFAULT_LOCALIZATION
 
 VERSION = "0.13.0"
